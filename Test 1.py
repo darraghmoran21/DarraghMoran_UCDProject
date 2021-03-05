1b35cd551ca071147fac954fd03548154ceb8696
darraghmoran21 <darragh.moran14@gmail.com>
2021-03-05 15:21:39 +0000

    Second Update

--- Test 1.py
+++ Test 1.py
@@ -2,4 +2,22 @@ import pandas as pd
 
 data=pd.read_csv("Summer-Olympic-medals-1976-to-2008.csv")
 
-print(data.head(5),data.shape)
+print(data.head())
+print(data.columns)
+print(data.index)
+print(data.values)
+data_ind = data.sort_values("Country")
+print(data_ind.head())
+Country=data["Country"]
+print(Country.head(20))
+year_2004 = data[data["Year"]>2004]
+print(year_2004)
+df=data['Year'].value_counts()
+print(df)
+medals_by_country=data.groupby("Country")["Medal"].count()
+print(medals_by_country)
+data_ind=data.set_index("City")
+print(data_ind)
+cities=["Beijing","Sydney"]
+print(data[data["City"].isin(cities)])
+print(data_ind.loc[cities])
