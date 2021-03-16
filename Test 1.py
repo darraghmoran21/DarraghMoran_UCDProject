d0806bd7798de6ae3995731538b607a0f40e6f59
darraghmoran21 <darragh.moran14@gmail.com>
2021-03-16 19:39:09 +0000

    Fourth Update

--- Test 1.py
+++ Test 1.py
@@ -26,18 +26,13 @@ Ireland_gender=Ireland['Gender'].value_counts()
 print(Ireland_gender)
 IrishMedals=Ireland.groupby('Sport')['Medal'].count().sort_values(ascending=False)
 print(IrishMedals)
-data_na=data.dropna()
-print(data_na)
-data_complete=data.fillna(0)
-print(data_complete)
 cities=["Beijing","Sydney"]
 print(data[data["City"].isin(cities)])
 print(data_ind.loc[cities])
 print(data.iloc[0:5,2:4])
 print(data.loc["2000":"2008"])
-data_na1=data.isnull().sum()
-print(data_na1)
-data.dropna(inplace=True)
+data[data.isnull().any(axis=1)]
+data.dropna(axis=0, inplace=True)
 data.Year=data.Year.astype(int)
 data[['City','Year']].drop_duplicates().reset_index(drop=True)
 for vars in data.columns:
@@ -58,6 +53,20 @@ avg = np.mean(np_medals)
 print("Average per country is :" + str(avg))
 med = np.median(np_medals)
 print("Median per country is :" + str(med))
+
+import matplotlib.pyplot as plt
+box = data[data.Sport == 'Boxing'].groupby(['Country']).Medal.size()
+top_10_boxing = box.sort_values(ascending = False)[:10]
+top_10_boxing_bar = top_10_boxing.plot.bar()
+plt.xticks(rotation=25)
+plt.xlabel('Medal won in Boxing (Top 10 Countries)')
+plt.ylabel('No of medals')
+
+import seaborn as sns
+usa= data[data['Country_Code']=='USA']
+sns.countplot(x='Year',data=usa)
+plt.title("Medals won by USA per year")
+plt.show()
 
 
 
